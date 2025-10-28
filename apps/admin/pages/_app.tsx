51b45c403d418ec4d4f48225900cfd8de4dcef1d
eo9on9 <eo9on9@gmail.com>
2025-10-28 22:33:11 +0900

    feat: set tailwind

--- apps/admin/pages/_app.tsx
+++ apps/admin/pages/_app.tsx
@@ -1,6 +1,12 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import { App as AppComponent } from '@/source/app/App'
+import type { AppProps } from 'next/app'
+
+import '@/styles/global.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <AppComponent>
+      <Component {...pageProps} />
+    </AppComponent>
+  )
 }
