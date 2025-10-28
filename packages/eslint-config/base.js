ca0f77979906a089b4c83459c753d7606d09b63c
eo9on9 <eo9on9@gmail.com>
2025-10-28 21:20:15 +0900

    feat: set lint and prettier

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -1,8 +1,8 @@
 import js from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
 import turboPlugin from "eslint-plugin-turbo";
 import tseslint from "typescript-eslint";
-import onlyWarn from "eslint-plugin-only-warn";
 
 /**
  * A shared ESLint configuration for the repository.
@@ -23,7 +23,10 @@ export const config = [
   },
   {
     plugins: {
-      onlyWarn,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      "prettier/prettier": "error",
     },
   },
   {
---
+++ packages/prettier-config/index.js
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Config} */
+export default {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  trailingComma: "all",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "avoid",
+  endOfLine: "lf",
+};
