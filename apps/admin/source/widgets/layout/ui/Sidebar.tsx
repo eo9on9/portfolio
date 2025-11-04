1aebc2485c58c5782df2f75a7922e362efe451a5
eo9on9 <eo9on9@gmail.com>
2025-11-04 13:52:32 +0900

    feat: Sidebar draft

---
+++ apps/admin/source/widgets/layout/ui/Sidebar.tsx
@@ -0,0 +1,210 @@
+import { Button } from '@shared/ui/Button'
+import { cva } from 'class-variance-authority'
+import {
+  FileText,
+  LayoutDashboard,
+  LogOut,
+  Package,
+  Pin,
+  Settings,
+  ShoppingCart,
+  Users,
+} from 'lucide-react'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
+
+export const Sidebar = () => {
+  const router = useRouter()
+  const [isPinned, setIsPinned] = useState(true)
+  const [isHover, setIsHover] = useState(false)
+
+  const isOpen = isPinned || isHover
+
+  const currentPath = router.pathname
+
+  return (
+    <div
+      className={containerCn({ isOpen })}
+      onMouseEnter={() => setIsHover(true)}
+      onMouseLeave={() => setIsHover(false)}
+    >
+      <div className="flex flex-col gap-4">
+        <div className="flex items-center justify-between">
+          <h1 className={titleCn({ isOpen })}>관리자 시스템</h1>
+          <Button
+            variant="ghost"
+            size="md"
+            onClick={() => setIsPinned(!isPinned)}
+          >
+            <Pin className={pinCn({ isPinned })} />
+          </Button>
+        </div>
+        <ul className="flex flex-col gap-1">
+          <li>
+            <Link
+              href="/"
+              className={linkCn({ isActive: currentPath === '/' })}
+            >
+              <span className={linkIconCn()}>
+                <LayoutDashboard className="w-4 h-4" />
+              </span>
+              <span className={linkTextCn({ isOpen })}>메인</span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/customer"
+              className={linkCn({ isActive: currentPath === '/customer' })}
+            >
+              <span className={linkIconCn()}>
+                <Users className="w-4 h-4" />
+              </span>
+              <span className={linkTextCn({ isOpen })}>고객 관리</span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/order"
+              className={linkCn({ isActive: currentPath === '/order' })}
+            >
+              <span className={linkIconCn()}>
+                <ShoppingCart className="w-4 h-4" />
+              </span>
+              <span className={linkTextCn({ isOpen })}>주문 관리</span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/product"
+              className={linkCn({ isActive: currentPath === '/product' })}
+            >
+              <span className={linkIconCn()}>
+                <Package className="w-4 h-4" />
+              </span>
+              <span className={linkTextCn({ isOpen })}>상품 관리</span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/report"
+              className={linkCn({ isActive: currentPath === '/report' })}
+            >
+              <span className={linkIconCn()}>
+                <FileText className="w-4 h-4" />
+              </span>
+              <span className={linkTextCn({ isOpen })}>보고서</span>
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/setting"
+              className={linkCn({ isActive: currentPath === '/setting' })}
+            >
+              <span className={linkIconCn()}>
+                <Settings className="w-4 h-4" />
+              </span>
+              <span className={linkTextCn({ isOpen })}>설정</span>
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className="flex flex-col gap-2 items-start">
+        <div className="flex items-center gap-2">
+          <div>
+            <div className="w-9 h-9 bg-gray-200 rounded-full" />
+          </div>
+          <div className={avatarTextCn({ isOpen })}>
+            <p className="text-sm text-gray-800 font-medium whitespace-nowrap">
+              John Doe
+            </p>
+            <p className="text-xs text-gray-500 whitespace-nowrap">
+              john.doe@example.com
+            </p>
+          </div>
+        </div>
+        <Button variant="ghost" size="md" className="flex items-center">
+          <LogOut className="w-4 h-4 mr-2" />
+          <span className={logoutTextCn({ isOpen })}>로그아웃</span>
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+const containerCn = cva(
+  'overflow-y-auto fixed top-0 left-0 bottom-0 flex flex-col justify-between gap-4 box-content p-4 border-r border-gray-200 bg-gray-100 transition-w duration-300 ease-out',
+  {
+    variants: {
+      isOpen: {
+        true: 'w-48',
+        false: 'w-9',
+      },
+    },
+  },
+)
+
+const titleCn = cva(
+  'overflow-hidden text-xl font-semibold text-gray-800 whitespace-nowrap transition-all duration-200 ease-out',
+  {
+    variants: {
+      isOpen: {
+        true: 'w-full opacity-100',
+        false: 'w-0 opacity-0',
+      },
+    },
+  },
+)
+
+const pinCn = cva('w-4 h-4 transition-all duration-200 ease-out', {
+  variants: {
+    isPinned: {
+      true: 'text-gray-800',
+      false: 'text-gray-400',
+    },
+  },
+})
+
+const linkCn = cva('flex items-center text-gray-800 rounded-sm', {
+  variants: {
+    isActive: {
+      true: 'bg-gray-200',
+      false: 'bg-transparent',
+    },
+  },
+})
+
+const linkIconCn = cva('flex items-center justify-center w-9 h-8')
+
+const linkTextCn = cva(
+  'overflow-hidden flex-1 block text-sm whitespace-nowrap transition-all duration-200 ease-out',
+  {
+    variants: {
+      isOpen: {
+        true: 'w-full opacity-100',
+        false: 'w-0 opacity-0',
+      },
+    },
+  },
+)
+
+const avatarTextCn = cva('transition-all duration-200 ease-out', {
+  variants: {
+    isOpen: {
+      true: 'w-full opacity-100',
+      false: 'w-0 opacity-0',
+    },
+  },
+})
+
+const logoutTextCn = cva(
+  'overflow-hidden flex-1 block text-sm whitespace-nowrap transition-all duration-200 ease-out',
+  {
+    variants: {
+      isOpen: {
+        true: 'w-full opacity-100',
+        false: 'w-0 opacity-0',
+      },
+    },
+  },
+)
