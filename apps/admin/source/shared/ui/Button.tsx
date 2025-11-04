e4e419a1981b54cb1d793a6dcf3dc034a79714c7
eo9on9 <eo9on9@gmail.com>
2025-11-04 09:02:48 +0900

    refactor: Button

--- apps/admin/source/shared/ui/Button.tsx
+++ apps/admin/source/shared/ui/Button.tsx
@@ -1,18 +1,22 @@
 import { cva } from 'class-variance-authority'
 
 interface ButtonProps {
-  variant: 'primary' | 'secondary' | 'ghost'
+  variant?: 'primary' | 'secondary' | 'ghost'
+  size?: 'md' | 'lg'
 }
 
 export const Button = ({
-  variant,
+  variant = 'primary',
+  size = 'md',
   children,
 }: React.PropsWithChildren<ButtonProps>) => {
-  return <button className={buttonVariants({ variant })}>{children}</button>
+  return (
+    <button className={buttonVariants({ variant, size })}>{children}</button>
+  )
 }
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center gap-2 h-10 px-4 font-medium text-sm rounded-sm cursor-pointer transition-bg duration-200 ease-out',
+  'inline-flex items-center justify-center gap-2 font-medium text-sm rounded-sm cursor-pointer transition-bg duration-200 ease-out',
   {
     variants: {
       variant: {
@@ -20,6 +24,10 @@ const buttonVariants = cva(
         secondary:
           'bg-white text-gray-800 border border-gray-800 hover:bg-gray-100',
         ghost: 'bg-transparent text-gray-800 hover:bg-gray-100',
+      },
+      size: {
+        md: 'h-8 px-2.5',
+        lg: 'h-9 px-3',
       },
     },
   },
