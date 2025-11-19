fdcf809abde7b4d7e61c194c08be3a7a82cb021e
eo9on9 <eo9on9@gmail.com>
2025-11-19 15:59:52 +0900

    feat: Input

---
+++ apps/marketplace/sources/shared/ui/Input.tsx
@@ -0,0 +1,38 @@
+import { cn } from '@shared/util/cn'
+import { tw } from '@shared/util/tw'
+import { cva } from 'class-variance-authority'
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  icon?: React.ReactNode
+  isError?: boolean
+}
+
+export const Input = ({ icon, className, isError, ...props }: InputProps) => {
+  return (
+    <div className={containerVariants({ isError, readOnly: !!props.readOnly })}>
+      {icon}
+      <input className={cn(inputTw, className)} {...props} />
+    </div>
+  )
+}
+
+const containerVariants = cva(
+  'inline-flex items-center gap-2 h-10 px-4 text-sm rounded-lg outline-1 focus-within:bg-white transition-outline duration-200 ease-out',
+  {
+    variants: {
+      isError: {
+        true: 'outline-error focus-within:outline-error',
+        false: 'outline-transparent focus-within:outline-gray-300',
+      },
+      readOnly: {
+        true: 'bg-white',
+        false: 'bg-gray-100 focus-within:bg-white',
+      },
+    },
+    defaultVariants: {
+      isError: false,
+    },
+  },
+)
+
+const inputTw = tw`flex-1 outline-none text-gray-800 placeholder:text-gray-400 read-only:text-gray-500 focus:ring-0`
