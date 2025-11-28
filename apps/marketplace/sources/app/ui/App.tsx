d43e117cab8a8d7c46393c7ab21cc93030598deb
eo9on9 <eo9on9@gmail.com>
2025-11-28 11:43:21 +0900

    feat: meta

--- apps/marketplace/sources/app/ui/App.tsx
+++ apps/marketplace/sources/app/ui/App.tsx
@@ -3,6 +3,7 @@ import { Guide } from '@app/ui/Guide'
 import { PusherProvider } from '@shared/hook/usePusher'
 import { ToastProvider } from '@shared/ui/Toast'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import Head from 'next/head'
 import { PropsWithChildren } from 'react'
 import { ErrorBoundary } from 'react-error-boundary'
 
@@ -10,15 +11,20 @@ const queryClient = new QueryClient()
 
 export const App = ({ children }: PropsWithChildren) => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ToastProvider>
-        <PusherProvider>
-          <ErrorBoundary FallbackComponent={AppErrorFallback}>
-            {children}
-            <Guide />
-          </ErrorBoundary>
-        </PusherProvider>
-      </ToastProvider>
-    </QueryClientProvider>
+    <>
+      <Head>
+        <title>아이템 거래소</title>
+      </Head>
+      <QueryClientProvider client={queryClient}>
+        <ToastProvider>
+          <PusherProvider>
+            <ErrorBoundary FallbackComponent={AppErrorFallback}>
+              {children}
+              <Guide />
+            </ErrorBoundary>
+          </PusherProvider>
+        </ToastProvider>
+      </QueryClientProvider>
+    </>
   )
 }
